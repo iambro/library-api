03bea9c59ed2c731b4cdd446754e8d9bad618117
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-04-04 16:59:12 +0200

    add default cover, add search function on enter

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,20 @@ import React from "react";
 
 const Search = props => {
   return (
-    <>
-      <input type="text" placeholder="Podaj tytuł..." value={props.search} onChange={props.change}/>
-      <button onClick={props.click}>Szukaj</button>
-    </>
+    <header className="header">
+      <form className="form" onSubmit={props.click}>
+        <input
+          className="form__input"
+          type="text"
+          placeholder="Podaj tytuł..."
+          value={props.search}
+          onChange={props.change}
+        />
+        <button className="form__button" type="submit">
+          Szukaj
+        </button>
+      </form>
+    </header>
   );
 };
 
