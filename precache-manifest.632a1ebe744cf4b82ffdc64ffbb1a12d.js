f676e2facb594e1109f59c91f713e7f782a14c81
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-04-12 17:02:34 +0200

    Updates

---
+++ precache-manifest.632a1ebe744cf4b82ffdc64ffbb1a12d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41088ff56a6db7fe0b91",
+    "url": "/library-api/static/css/main.5c203248.chunk.css"
+  },
+  {
+    "revision": "41088ff56a6db7fe0b91",
+    "url": "/library-api/static/js/main.e3d3de2f.chunk.js"
+  },
+  {
+    "revision": "62e055c0e2c9f285318a",
+    "url": "/library-api/static/js/runtime~main.a187e1f0.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/library-api/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "97ce854c1aa7e5e9c6eb75e92ff5805c",
+    "url": "/library-api/static/media/nocover.97ce854c.png"
+  },
+  {
+    "revision": "5307bf2a6d0c2d20b88b72db42878e19",
+    "url": "/library-api/index.html"
+  }
+];
